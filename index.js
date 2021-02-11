63e0856cd6b6faa01e88e43acbb2225b151b5e69
Samuel Wilson <samwil56@gmail.com>
2021-02-11 00:19:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function updateObjectWithKeyAndValue (object, key, value) {
   var newObj = Object.assign({},object,key,value)
   return newObj
+  return object
 }
 
 function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
